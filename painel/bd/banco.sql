af02d23e9ba92b9040e07891799c375d83a0f6c0
PROGR WEB - MANHÃ <PROGR WEB - MANH@MININT-9SOJ8BE>
2021-02-18 17:37:33 -0200

    Arquivo de banco de dados incluso na pasta /painel/bd

---
+++ painel/bd/banco.sql
@@ -0,0 +1,112 @@
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,53
+ColsVisible_connform.ListSessions<|||>1<|||>0
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+CoolBand0Index<|||>3<|||>0
+CoolBand0Break<|||>3<|||>1
+CoolBand0Width<|||>3<|||>928
+CoolBand1Index<|||>3<|||>1
+CoolBand1Break<|||>3<|||>1
+CoolBand1Width<|||>3<|||>928
+MainWinOnMonitor<|||>3<|||>0
+MainWinTop<|||>3<|||>80
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,71
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+LastSessions<|||>1<|||>Laragon
+LastActiveSession<|||>1<|||>Laragon
+ColWidths_frmTableEditor.listColumns<|||>1<|||>38,110,156,90,120,134,50,239,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>389,100,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>442,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+MainWinLeft<|||>3<|||>254
+MainWinMaximized<|||>3<|||>1
+MemoEditorWidth<|||>3<|||>136
+sqloutheight<|||>3<|||>63
+FilterPanel<|||>3<|||>0
+SessionManager_WindowLeft<|||>3<|||>305
+SessionManager_WindowTop<|||>3<|||>202
+dbtreewidth<|||>3<|||>396
+MainWinWidth<|||>3<|||>993
+MainWinHeight<|||>3<|||>560
+DatabaseFilter<|||>1<|||>painel<{{{><}}}>
+DateTimeEditor_CursorPos_Type4<|||>3<|||>0
+querymemoheight<|||>3<|||>152
+SQLFile0<|||>1<|||>C:\Users\PROGR WEB - MANHÃ\Documents\oiiii.sql
+SQLFile1<|||>1<|||>C:\Users\PROGR WEB - MANHÃ\Documents\dados.sql
+SQLFile2<|||>1<|||>C:\laragon\bin\heidisql\Backups\query-tab-2021-01-25_19-09-43-185.sql
+Servers\Laragon\SessionCreated<|||>1<|||>2017-02-14 13:41:54
+Servers\Laragon\Host<|||>1<|||>127.0.0.1
+Servers\Laragon\WindowsAuth<|||>3<|||>0
+Servers\Laragon\User<|||>1<|||>root
+Servers\Laragon\Password<|||>1<|||>6
+Servers\Laragon\LoginPrompt<|||>3<|||>0
+Servers\Laragon\Port<|||>1<|||>3306
+Servers\Laragon\NetType<|||>3<|||>0
+Servers\Laragon\Compressed<|||>3<|||>0
+Servers\Laragon\LocalTimeZone<|||>3<|||>0
+Servers\Laragon\QueryTimeout<|||>3<|||>0
+Servers\Laragon\KeepAlive<|||>3<|||>20
+Servers\Laragon\FullTableStatus<|||>3<|||>1
+Servers\Laragon\Databases<|||>1<|||>
+Servers\Laragon\Comment<|||>1<|||>
+Servers\Laragon\StartupScriptFilename<|||>1<|||>
+Servers\Laragon\SSHtunnelHost<|||>1<|||>
+Servers\Laragon\SSHtunnelHostPort<|||>3<|||>0
+Servers\Laragon\SSHtunnelUser<|||>1<|||>
+Servers\Laragon\SSHtunnelPassword<|||>1<|||>5
+Servers\Laragon\SSHtunnelTimeout<|||>3<|||>4
+Servers\Laragon\SSHtunnelPrivateKey<|||>1<|||>
+Servers\Laragon\SSHtunnelPort<|||>3<|||>3307
+Servers\Laragon\SSL_Active<|||>3<|||>0
+Servers\Laragon\SSL_Key<|||>1<|||>
+Servers\Laragon\SSL_Cert<|||>1<|||>
+Servers\Laragon\SSL_CA<|||>1<|||>
+Servers\Laragon\SSL_Cipher<|||>1<|||>
+Servers\Laragon\RefusedCount<|||>3<|||>4
+Servers\Laragon\ServerVersionFull<|||>1<|||>5.7.24 - MySQL Community Server (GPL)
+Servers\Laragon\ConnectCount<|||>3<|||>26
+Servers\Laragon\ServerVersion<|||>3<|||>50724
+Servers\Laragon\LastConnect<|||>1<|||>2021-02-18 15:33:17
+Servers\Laragon\lastUsedDB<|||>1<|||>paineladm1
+Servers\Laragon\CleartextPluginEnabled<|||>3<|||>0
+Servers\Laragon\TreeBackground<|||>3<|||>536870911
+Servers\Laragon\paineladm1|contato\Sort<|||>1<|||>0_mensagem|
+Servers\Laragon\paineladm1|contatos\Sort<|||>1<|||>0_id|0_nome|0_email|
+Servers\Laragon\paineladm1|produtos\Sort<|||>1<|||>0_id|1_nome|
+Servers\Laragon\paineladm1|servicos\Sort<|||>1<|||>0_id|1_nome|
+Servers\Laragon\projetofinal|contato\Sort<|||>1<|||>1_id_contato|
+Servers\Laragon\QueryHistory\2<|||>1<|||>2021-01-26 10:35:00|paineladm1|0|UPDATE  produtos<{{{><}}}>SET <{{{><}}}>nome ='Camisa polo listrada',<{{{><}}}>tipo ='vestuario masc',<{{{><}}}>valor = 420 <{{{><}}}>WHERE id = 2
+Servers\Laragon\QueryHistory\1<|||>1<|||>2021-01-26 10:36:51|paineladm1|0|/*UPDATE  produtos<{{{><}}}>SET <{{{><}}}>nome ='Camisa polo listrada',<{{{><}}}>tipo ='vestuario masc',<{{{><}}}>valor = 420 <{{{><}}}>WHERE id = '2'<{{{><}}}>*/<{{{><}}}><{{{><}}}>SELECT * FROM produtos
+Servers\Laragon\QueryHistory\4<|||>1<|||>2021-01-26 10:37:53|paineladm1|0|/*UPDATE  produtos<{{{><}}}>SET <{{{><}}}>nome ='C. listrada',<{{{><}}}>tipo ='vestuario masc',<{{{><}}}>valor = 420 <{{{><}}}>WHERE id = '2'<{{{><}}}>*/<{{{><}}}><{{{><}}}>SELECT * FROM produtos
+Servers\Laragon\QueryHistory\3<|||>1<|||>2021-01-26 11:52:18|paineladm1|31|UPDATE  produtos<{{{><}}}>SET <{{{><}}}>nome ='C listrada',<{{{><}}}>tipo ='vestuario masc',<{{{><}}}>valor = 420 <{{{><}}}>WHERE id = 1
+Servers\Laragon\QueryHistory\5<|||>1<|||>2021-01-27 18:37:24|paineladm1|47|UPDATE  contato<{{{><}}}>SET <{{{><}}}>nome ='Oliver Pontes',<{{{><}}}>email ='oliver2312@outook.com',<{{{><}}}>mensagem = 'olá' <{{{><}}}>WHERE id = 1
